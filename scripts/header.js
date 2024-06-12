1ac5bcefcecad80f0a47ba15fc432895ee2c1be6
Emerson <emersonmedina42@gmail.com>
2024-06-12 14:59:24 -0300

    inicio pg avances

--- scripts/header.js
+++ scripts/header.js
@@ -7,11 +7,13 @@ const navToggle = document.getElementById('nav-toggle')
 if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
+        navToggle.style.display = 'none';
     })
 }
 
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
+        navToggle.style.display = 'block';
     })
 }
