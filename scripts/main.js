377bf51346b5c711c8ab981304921555c1d78fb3
Elio Agustin Albornoz <91352723+agustinAlbornoz@users.noreply.github.com>
2024-06-12 22:33:07 -0300

    Integración en juegos - cambios en integrantes

--- scripts/main.js
+++ scripts/main.js
@@ -69,7 +69,7 @@ function verificarRespuesta(seleccionada) {
         Swal.fire({
             icon: 'info',
             title: 'Fin del juego',
-            text: 'obtuviste ' + puntos + 'respuestas correctas',
+            text: 'obtuviste ' + puntos + ' respuestas correctas',
             confirmButtonText: 'Repetir Juego'
         }).then((result) => {
             if (result.isConfirmed) {
@@ -86,3 +86,29 @@ function reiniciarJuego() {
 }
 
 cargarSiguienteImagen();
+
+
+
+// SCRIPTS PAGINA JUEGOS
+
+function loadGame(game) {
+    const gameContainer = document.getElementById('game-container');
+    
+    // Limpia el contenido actual del contenedor de juegos
+    gameContainer.innerHTML = '';
+
+    // Carga el juego correspondiente
+    switch(game) {
+        case 'game1':
+            gameContainer.innerHTML = '<iframe src="../pages/juego-albornoz.html" frameborder="0" width="100%" height="100%"></iframe>';
+            break;
+        case 'game2':
+            gameContainer.innerHTML = '<iframe src="../pages/juego-emerson.html" frameborder="0" width="100%" height="100%"></iframe>';
+            break;
+        case 'game3':
+            gameContainer.innerHTML = '<iframe src="game3.html" frameborder="0" width="100%" height="100%"></iframe>';
+            break;
+        default:
+            gameContainer.innerHTML = '<p>Selecciona un juego para empezar a jugar.</p>';
+    }
+}
